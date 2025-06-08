39e5c7b162d8fe3b49770697e0775eac9e66de79
Ayomide650 <ahmedayomide1002@gmail.com>
2025-06-08 19:07:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/Sensi-Gen-2.0/',
   build: {
-    outDir: 'docs'
-  },
-  // ...existing config...
+    outDir: 'docs',
+    emptyOutDir: false  // Don't delete Jekyll files
+  }
 });
